929c24813654edf22573e991a54ab005045bb786
Devadavid <s3927112@rmit.edu.vn>
2022-06-07 12:42:27 +0700

    new update

--- PersonalWebsite/app.js
+++ PersonalWebsite/app.js
@@ -1,42 +1,17 @@
-let btn1 = document.querySelector("#btn1");
-let btn2 = document.querySelector("#btn2");
-let btn3 = document.querySelector("#btn3");
-let btn4 = document.querySelector("#btn4");
-let btn2prev = document.querySelector("#btn2prev");
-let btn3prev = document.querySelector("#btn3prev");
+const openBtn = document.querySelector('.open-btn');
+const closeBtn = document.querySelector('.close-btn');
+const wrap = document.querySelectorAll('.wrap');
 
-
-
-
-let arrow1 = document.querySelector("#arrow1");
-let arrow2 = document.querySelector("#arrow2");
-let arrow3 = document.querySelector("#arrow3");
-let arrow4 = document.querySelector("#arrow4");
-let arrow5 = document.querySelector("#arrow5");
-let arrow6 = document.querySelector("#arrow6");
-
-
-
-arrow1.addEventListener('click', function(){
-    btn1.click();
+openBtn.addEventListener('click', function(){
+    for(let i = 0; i < wrap.length; i++) {
+        wrap[i].classList.toggle('visible');
+    }
 })
 
-arrow2.addEventListener('click', function(){
-    btn2.click();
-})
-
-arrow3.addEventListener('click', function(){
-    btn3.click();
-})
-
-arrow4.addEventListener('click', function(){
-    btn4.click();
-})
-arrow5.addEventListener('click', function(){
-    btn2prev.click();
-})
-arrow6.addEventListener('click', function(){
-    btn3prev.click();
+closeBtn.addEventListener('click', function(){
+    for(let i = 0; i < wrap.length; i++) {
+        wrap[i].classList.toggle('visible');
+    }
 })
 
 
