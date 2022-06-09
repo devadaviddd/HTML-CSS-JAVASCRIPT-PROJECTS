e593c80f2578d60a73d3cf243b1024690169ea4a
Devadavid <s3927112@rmit.edu.vn>
2022-06-09 15:35:09 +0700

    update skills effect

--- PersonalWebsite/app.js
+++ PersonalWebsite/app.js
@@ -1,6 +1,23 @@
 const openBtn = document.querySelector('.open-btn');
 const closeBtn = document.querySelector('.close-btn');
 const wrap = document.querySelectorAll('.wrap');
+const skills = document.querySelectorAll('.skills');
+
+function checkSkills() {
+    const triggerBottom = window.innerHeight / 5 * 4;
+
+    skills.forEach(skill => {
+        const boxTop = skill.getBoundingClientRect().top;
+
+        if(boxTop < triggerBottom) {
+            skill.classList.add('show');
+        } else {
+            skill.classList.remove('show');
+        }
+    })
+}
+
+window.addEventListener('scroll', checkSkills);
 
 openBtn.addEventListener('click', function(){
     for(let i = 0; i < wrap.length; i++) {
@@ -13,7 +30,6 @@ closeBtn.addEventListener('click', function(){
         wrap[i].classList.toggle('visible');
     }
 })
-
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
